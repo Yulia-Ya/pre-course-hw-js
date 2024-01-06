8033b54c595a2b9ef60d2da3d18dae891f131503
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-06 22:31:14 +0300

    switch case-6

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -7,7 +7,10 @@ case rule.length < 25:
   case rule.length > 25: 
   console.log("У меня 100 пудов все получится"); 
   break;
+  case rule.length == 25: 
+  console.log("50 на 50"); 
+  break;
 default:
-console.log("50 на 50");
-break;
+console.log("Ничего не подошло");
+
 }
