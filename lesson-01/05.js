02155d93bb23f97ce9362078745c08b3f9b83333
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-06 22:44:05 +0300

    switch case-8

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -2,10 +2,10 @@ let rule = "Еще не родился тот человек, который, п
 
 switch (true) { 
 case rule.length < 25: 
-  console.log("Все таки нет правил без исключения"); 
+  console.log("У меня 100 пудов все получится"); 
   break; 
   case rule.length > 25: 
-  console.log("У меня 100 пудов все получится"); 
+  console.log("Все таки нет правил без исключения"); 
   break;
   case rule.length == 25: 
   console.log("50 на 50"); 
