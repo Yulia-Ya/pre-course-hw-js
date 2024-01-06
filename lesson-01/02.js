b293fbb876d3d66e14777d68c719985d685ebcab
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-06 21:33:03 +0300

    random num

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,14 @@
-// code
+function randomNum (){
+    let number1 = Math.floor(Math.random() * 100)
+    // return number1
+    
+    if (number1 % 2 == 0) {
+      return  `${number1}:Это четное число`;
+    } else {
+      return `${number1}:Это нечетное число`
+    
+    }
+    
+    }
+    
+    console.log(randomNum())
