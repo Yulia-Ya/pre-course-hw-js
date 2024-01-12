17a751eea28c404474373596662bb8e784fd80d1
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-12 15:45:33 +0300

    debugger

---
+++ lesson-02/02.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+
+        return n * factorial(n - 1);
+    }
+    debbuger;
+}
+console.log(factorial(5));
