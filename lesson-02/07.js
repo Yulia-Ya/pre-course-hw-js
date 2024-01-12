cdd6e21e6dbbfa7d76d7a15620eb7566862ea2ef
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-12 18:09:17 +0300

    copy obj married

---
+++ lesson-02/07.js
@@ -0,0 +1,21 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let marriedPerson = {
+    name: "Petr",
+    surname: "Petrov",
+    married: true,
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+console.log(passportWithAddress);
+console.log(marriedPerson);
