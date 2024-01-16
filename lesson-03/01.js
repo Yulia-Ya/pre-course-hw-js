af247ff721cf764cb0795c7f65cfb2a610a93a4b
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-01-16 20:42:48 +0300

    task 1 week 3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((colors) => colors === "черный" || colors === "красный" || colors === "желтый" ).join("-")
+}
+
+console.log(createColorString());
